e83602a60eadea9c75d995f1512410c60d3c5b33
Ken Atopos <z1158229761@outlook.com>
2023-02-16 00:46:56 +0000

    first python portfolio project, text to morse code converter

---
+++ main.py
@@ -0,0 +1,28 @@
+morse_code = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
+    'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
+    'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---',
+    'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
+    'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--',
+    'Z': '--..', '0': '-----', '1': '.----', '2': '..---', '3': '...--',
+    '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..',
+    '9': '----.', ' ': ' '
+}
+
+
+def text_to_morse(text_origin):
+    morse_origin = ''
+    for char in text_origin.upper():
+        if char in morse_code:
+            morse_origin += morse_code[char] + ' '
+        else:
+            morse_origin += ' '
+    return morse_origin
+
+
+text = input("Please enter something to be converted: ")
+
+morse = text_to_morse(text)
+print(morse)
+
+
